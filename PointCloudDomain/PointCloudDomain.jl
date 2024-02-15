cb577ca382ffd4e0b5fa0fb4644941c7176aece8
jarias9 <68081892+jarias9@users.noreply.github.com>
2024-02-15 16:02:59 -0500

    Update PointCloudDomain

--- PointCloudDomain/PointCloudDomain.jl
+++ PointCloudDomain/PointCloudDomain.jl
@@ -1,21 +1,21 @@
 using Revise
 using StaticArrays
 
+struct BoundaryData{Ti<:Integer,Tv<:SVector{N,T} where {N,T<:Number}}
+    idx::Vector{Ti}       # Indices of boundary points
+    normals::Vector{Tv}   # Normals at boundary points
+end
+
 struct PointCloudDomain{Dim,Tv,Ti}
     points::Vector{Tv}  # Point coordinates
     neighbors::Vector{Vector{Ti}}  # Neighbors for each point
-    boundary_tags::Dict{Symbol,Tuple{Vector{Ti},Vector{Tv}}}  # Boundary tags with indices and normals of boundary points
+    boundary_tags::Dict{Symbol,BoundaryData{Ti,Tv}}  # Adjusted to use BoundaryData
 end
 
-# Outer constructor
-# function PointCloudDomain(points::Vector{Tv}, neighbors::Vector{Vector{Ti}},
-#     boundary_tags::Dict{Symbol,Tuple{Vector{Ti},Vector{Tv}}}) where {Tv<:SVector{N,Float64},Ti}
-#     return PointCloudDomain{N,Tv,Ti}(points, neighbors, boundary_tags)
-# end
-
-PointCloudDomain(points::Vector{Tv}, neighbors::Vector{Vector{Ti}},
-    boundary_tags::Dict{Symbol,Tuple{Vector{Ti},Vector{Tv}}}) where {N,Tv<:SVector{N,Float64},Ti} =
-    PointCloudDomain{N,Tv,Ti}(points, neighbors, boundary_tags)
+function PointCloudDomain(points::Vector{Tv}, neighbors::Vector{Vector{Ti}},
+    boundary_tags::Dict{Symbol,BoundaryData{Ti,Tv}}) where {N,Tv<:SVector{N,Float64},Ti}
+    return PointCloudDomain{N,Tv,Ti}(points, neighbors, boundary_tags)
+end
 
 
 
--- PointCloudDomain/PointCloudDomain_test.jl
+++ PointCloudDomain/PointCloudDomain_test.jl
@@ -4,8 +4,8 @@ includet("PointCloudDomain.jl")
 
 # Example boundary tags with indices and normals
 boundary_tags = Dict(
-    :top => ([1, 2, 3], [SVector{3}(0.0, 1.0, 0.0), SVector{3}(0.0, 1.0, 0.0), SVector{3}(0.0, 1.0, 0.0)]),
-    :side => ([4, 5], [SVector{3}(1.0, 0.0, 0.0), SVector{3}(1.0, 0.0, 0.0)])
+    :top => BoundaryData([1, 2, 3], [SVector{3}(0.0, 1.0, 0.0), SVector{3}(0.0, 1.0, 0.0), SVector{3}(0.0, 1.0, 0.0)]),
+    :side => BoundaryData([4, 5], [SVector{3}(1.0, 0.0, 0.0), SVector{3}(1.0, 0.0, 0.0)])
 )
 
 # Example initialization of PointCloudMeshData
@@ -15,8 +15,8 @@ pcmd = PointCloudDomain(points, neighbors, boundary_tags)
 
 # Example boundary tags with indices and normals
 boundary_tags = Dict(
-    :top => ([1, 2, 3], [SVector{2}(0.0, 1.0), SVector{2}(0.0, 1.0), SVector{2}(0.0, 1.0)]),
-    :side => ([4, 5], [SVector{2}(1.0, 0.0), SVector{2}(1.0, 0.0)])
+    :top => BoundaryData([1, 2, 3], [SVector{2}(0.0, 1.0), SVector{2}(0.0, 1.0), SVector{2}(0.0, 1.0)]),
+    :side => BoundaryData([4, 5], [SVector{2}(1.0, 0.0), SVector{2}(1.0, 0.0)])
 )
 
 # Example initialization of PointCloudMeshData
