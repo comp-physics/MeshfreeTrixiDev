2d7b309db037a633ebd3fac3079717b841b8c773
jarias9 <68081892+jarias9@users.noreply.github.com>
2024-02-19 15:52:39 -0500

    Update geometry_primatives.jl
    Reworking required fields. Will also add basis functions (f) here

--- PointCloudDomain/geometry_primatives.jl
+++ PointCloudDomain/geometry_primatives.jl
@@ -43,78 +43,19 @@ function ConstructionBase.getproperties(rd::RefElemData)
      pv = rd.pv)
 end
 
-_propertynames(::Type{RefElemData}, private::Bool = false) = (:num_faces, :Np, :Nq, :Nfq)
-function Base.propertynames(x::RefElemData{1}, private::Bool = false)
-    return (fieldnames(RefElemData)..., _propertynames(RefElemData)...,
-            :r, :rq, :rf, :rp, :nrJ, :Dr)
-end
-function Base.propertynames(x::RefElemData{2}, private::Bool = false)
-    return (fieldnames(RefElemData)..., _propertynames(RefElemData)...,
-            :r, :s, :rq, :sq, :rf, :sf, :rp, :sp, :nrJ, :nsJ, :Dr, :Ds)
-end
-function Base.propertynames(x::RefElemData{3}, private::Bool = false)
-    return (fieldnames(RefElemData)..., _propertynames(RefElemData)...,
-            :r, :s, :t, :rq, :sq, :tq, :rf, :sf, :tf,
-            :rp, :sp, :tp, :nrJ, :nsJ, :ntJ, :Dr, :Ds, :Dt)
+# Updated _propertynames function to reflect the new fields in RefElemData
+_propertynames(::Type{RefElemData}, private::Bool = false) = (:nv, :pv)
+
+function Base.propertynames(x::RefElemData, private::Bool = false)
+    return (fieldnames(typeof(x))..., _propertynames(typeof(x))...)
 end
 
 # convenience unpacking routines
-function Base.getproperty(x::RefElemData{Dim, ElementType, ApproxType},
-                          s::Symbol) where {Dim, ElementType, ApproxType}
-    if s == :r
-        return getfield(x, :rst)[1]
-    elseif s == :s
-        return getfield(x, :rst)[2]
-    elseif s == :t
-        return getfield(x, :rst)[3]
-
-    elseif s == :rq
-        return getfield(x, :rstq)[1]
-    elseif s == :sq
-        return getfield(x, :rstq)[2]
-    elseif s == :tq
-        return getfield(x, :rstq)[3]
-
-    elseif s == :rf
-        return getfield(x, :rstf)[1]
-    elseif s == :sf
-        return getfield(x, :rstf)[2]
-    elseif s == :tf
-        return getfield(x, :rstf)[3]
-
-    elseif s == :rp
-        return getfield(x, :rstp)[1]
-    elseif s == :sp
-        return getfield(x, :rstp)[2]
-    elseif s == :tp
-        return getfield(x, :rstp)[3]
-
-    elseif s == :nrJ
-        return getfield(x, :nrstJ)[1]
-    elseif s == :nsJ
-        return getfield(x, :nrstJ)[2]
-    elseif s == :ntJ
-        return getfield(x, :nrstJ)[3]
-
-    elseif s == :Dr
-        return getfield(x, :Drst)[1]
-    elseif s == :Ds
-        return getfield(x, :Drst)[2]
-    elseif s == :Dt
-        return getfield(x, :Drst)[3]
-
-    elseif s == :Nfaces || s == :num_faces
-        return num_faces(getfield(x, :element_type))
-    elseif s == :Np
-        return length(getfield(x, :rst)[1])
-    elseif s == :Nq
-        return length(getfield(x, :rstq)[1])
-    elseif s == :Nfq
-        return length(getfield(x, :rstf)[1])
-    else
-        return getfield(x, s)
-    end
-end
+# Not necessary for PointCloudDomain
+# function Base.getproperty(x::RefElemData{Dim, ElementType, ApproxType},
+#                           s::Symbol) where {Dim, ElementType, ApproxType}
+#     return getfield(x, s)
+# end
 
 """
     function RefElemData(elem; N, kwargs...)
@@ -126,74 +67,34 @@ RefElemData(elem; N, kwargs...) = RefElemData(elem, N; kwargs...)
 RefElemData(elem, approx_type; N, kwargs...) = RefElemData(elem, approx_type, N; kwargs...)
 
 # default to Polynomial-type RefElemData
-RefElemData(elem, N::Int; kwargs...) = RefElemData(elem, Polynomial(), N; kwargs...)
+RefElemData(elem, N::Int; kwargs...) = RefElemData(elem, RBF(), N; kwargs...)
 
 @inline Base.ndims(::Point1D) = 1
 @inline Base.ndims(::Point2D) = 2
 @inline Base.ndims(::Point3D) = 3
-
-# @inline num_vertices(::Tri) = 3
-# @inline num_vertices(::Union{Quad, Tet}) = 4
-# @inline num_vertices(::Hex) = 8
-# @inline num_vertices(::Wedge) = 6
-# @inline num_vertices(::Pyr) = 5
-
-# @inline num_faces(::Line) = 2
-# @inline num_faces(::Tri) = 3
-# @inline num_faces(::Union{Quad, Tet}) = 4
-# @inline num_faces(::Union{Wedge, Pyr}) = 5
-# @inline num_faces(::Hex) = 6
-
-# @inline face_type(::Union{Tri, Quad}) = Line()
-# @inline face_type(::Hex) = Quad()
-# @inline face_type(::Tet) = Tri()
-
-# generic fallback 
-# @inline face_type(elem::AbstractElemShape, id) = face_type(elem)
-
-# Wedges have different types of faces depending on the face. 
-# We define the first three faces to be quadrilaterals and the 
-# last two faces are triangles.
-# @inline face_type(::Wedge, id) = (id <= 3) ? Quad() : Tri()
-
-# Pyramids have different types of faces depending on the face. 
-# We define the first four faces to be triangles and the 
-# last face to be a quadrilateral. 
-# @inline face_type(::Pyr, id) = (id <= 4) ? Tri() : Quad()
 
 # ====================================================
 #          RefElemData approximation types
 # ====================================================
 
 """
-    Polynomial{T}
+    RBF{T}
 
-Represents polynomial approximation types (as opposed to finite differences). 
-By default, `Polynomial()` constructs a `Polynomial{StartUpDG.DefaultPolynomialType}`.
-Specifying a type parameters allows for dispatch on additional structure within a
-polynomial approximation (e.g., collocation, tensor product quadrature, etc). 
+Represents RBF approximation types (as opposed to generic polynomials). 
+By default, `RBF()` constructs a `RBF{DefaultRBFType}`.
+Specifying a type parameters allows for dispatch on additional structure within an
+RBF approximation (e.g., polyharmonic spline, collocation, etc). 
 """
-struct Polynomial{T}
+struct RBF{T}
     data::T
 end
 
-struct DefaultPolynomialType end
-Polynomial() = Polynomial{DefaultPolynomialType}(DefaultPolynomialType())
-
-"""
-    TensorProductQuadrature{T}
-
-A type parameter to `Polynomial` indicating that 
-"""
-struct TensorProductQuadrature{T}
-    quad_rule_1D::T  # 1D quadrature nodes and weights (rq, wq)
-end
-
-TensorProductQuadrature(r1D, w1D) = TensorProductQuadrature((r1D, w1D))
+struct DefaultRBFType end
+RBF() = RBF{DefaultRBFType}(DefaultRBFType())
 
 # Polynomial{Gauss} type indicates (N+1)-point Gauss quadrature on tensor product elements
-struct Gauss end
-Polynomial{Gauss}() = Polynomial(Gauss())
+struct PolyharmonicSpline end
+RBF{PolyharmonicSpline}() = RBF(PolyharmonicSpline())
 
 # ====================================
 #              Printing 
@@ -214,11 +115,22 @@ end
 
 _short_typeof(x) = typeof(x)
 
-_short_typeof(approx_type::Wedge) = "Wedge"
-_short_typeof(approx_type::Pyr) = "Pyr"
+_short_typeof(approx_type::RBF{<:DefaultRBFType}) = "RBF"
+_short_typeof(approx_type::RBF{<:PolyharmonicSpline}) = "RBF{PolyharmonicSpline}"
+# function _short_typeof(approx_type::Polynomial{<:TensorProductQuadrature})
+#     "Polynomial{TensorProductQuadrature}"
+# end
 
-_short_typeof(approx_type::Polynomial{<:DefaultPolynomialType}) = "Polynomial"
-_short_typeof(approx_type::Polynomial{<:Gauss}) = "Polynomial{Gauss}"
-function _short_typeof(approx_type::Polynomial{<:TensorProductQuadrature})
-    "Polynomial{TensorProductQuadrature}"
-end
+"""
+    RefElemData(elem::Line, N;
+                quad_rule_vol = quad_nodes(elem, N+1))
+    RefElemData(elem::Union{Tri, Quad}, N;
+                 quad_rule_vol = quad_nodes(elem, N),
+                 quad_rule_face = gauss_quad(0, 0, N))
+    RefElemData(elem::Union{Hex, Tet}, N;
+                 quad_rule_vol = quad_nodes(elem, N),
+                 quad_rule_face = quad_nodes(Quad(), N))
+    RefElemData(elem; N, kwargs...) # version with keyword args
+
+Constructor for `RefElemData` for different element types.
+"""
