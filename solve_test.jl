6b2ebb85711cd7bb6c4cb97cd792edbeba75a855
jarias9 <68081892+jarias9@users.noreply.github.com>
2024-04-05 20:57:54 -0400

    Smooth cyl test
    have to actually increase the scalar multiplier on SourceHyperviscosityTominec to prevent oscillations

--- solve_test.jl
+++ solve_test.jl
@@ -5,7 +5,7 @@ using OrdinaryDiffEq
 # includet("../header.jl")
 
 # Base Methods
-approximation_order = 5
+approximation_order = 3
 rbf_order = 3
 # Specialized Methods
 basis = PointCloudBasis(Point2D(), approximation_order;
@@ -15,26 +15,27 @@ solver = PointCloudSolver(basis)
 dir = "./medusa_point_clouds"
 casename = "cyl_0_0125"
 domain_name = joinpath(dir, casename)
+savename = casename * "_order_$approximation_order"
 boundary_names = Dict(:inlet => 1, :outlet => 2, :bottom => 3, :top => 4, :cyl => 5)
 domain = PointCloudDomain(solver, domain_name, boundary_names)
 
 # Instantiate Semidiscretization
-function basic_limiter!(u_ode, integrator,
-                        semi::Trixi.AbstractSemidiscretization,
-                        t)
-    @unpack mesh, solver, cache, equations = semi
-    for e in eachelement(mesh, solver, cache)
-        rho, rho_v1, rho_v2, rho_e = u_ode[e]
-        if rho < 0.0
-            rho = eps()
-        end
-        if rho_e < 0.0
-            rho_e = eps()
-        end
-        # p = (equations.gamma - 1) * (rho_e - 0.5 * (rho_v1 * v1 + rho_v2 * v2))
-        u_ode[e] = SVector(rho, rho_v1, rho_v2, rho_e)
-    end
-end
+# function basic_limiter!(u_ode, integrator,
+#                         semi::Trixi.AbstractSemidiscretization,
+#                         t)
+#     @unpack mesh, solver, cache, equations = semi
+#     for e in eachelement(mesh, solver, cache)
+#         rho, rho_v1, rho_v2, rho_e = u_ode[e]
+#         if rho < 0.0
+#             rho = eps()
+#         end
+#         if rho_e < 0.0
+#             rho_e = eps()
+#         end
+#         # p = (equations.gamma - 1) * (rho_e - 0.5 * (rho_v1 * v1 + rho_v2 * v2))
+#         u_ode[e] = SVector(rho, rho_v1, rho_v2, rho_e)
+#     end
+# end
 equations = CompressibleEulerEquations2D(1.4)
 function initial_condition_cyl(x, t, equations::CompressibleEulerEquations2D)
     rho = 1.4
@@ -62,8 +63,11 @@ boundary_conditions = (; :inlet => BoundaryConditionDirichlet(initial_condition)
                        :cyl => boundary_condition_slip_wall)
 
 # Test upwind viscosity
+# source_rv = SourceResidualViscosityTominec(solver, equations, domain; c_rv = 1.0,
+#                                            c_uw = 1.0, polydeg = approximation_order)
 source_rv = SourceUpwindViscosityTominec(solver, equations, domain; c_uw = 1.0)
-source_hv2 = SourceHyperviscosityTominec(solver, equations, domain; c = 1.0)
+source_hv2 = SourceHyperviscosityTominec(solver, equations, domain;
+                                         c = domain.pd.dx_min^(-2))
 sources = SourceTerms(hv = source_hv2, rv = source_rv)
 semi = SemidiscretizationHyperbolic(domain, equations,
                                     initial_condition, solver;
@@ -76,14 +80,13 @@ ode = semidiscretize(semi, tspan)
 # summary_callback = SummaryCallback()
 summary_callback = InfoCallback()
 alive_callback = AliveCallback(alive_interval = 10)
+# history_callback = HistoryCallback(approx_order = approximation_order)
 # analysis_interval = 100
 # analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
-save_solution = SolutionSavingCallback(interval = 10,
-                                       prefix = casename)
-# save_solution = SaveSolutionCallback(interval = 100,
-#                                      save_initial_solution = true,
-#                                      save_final_solution = true,
-#                                      solution_variables = cons2prim)
+save_solution = SolutionSavingCallback(dt = 0.01,
+                                       prefix = savename)
+# save_solution = SolutionSavingCallback(interval = 10,
+#                                        prefix = savename)
 callbacks = CallbackSet(summary_callback, alive_callback, save_solution)
 time_int_tol = 1e-4
 stage_limiter! = PositivityPreservingLimiterZhangShu(thresholds = (5.0e-7, 1.0e-6),
@@ -96,6 +99,7 @@ sol = solve(ode, SSPRK43(stage_limiter! = stage_limiter!); abstol = time_int_tol
 #             abstol = time_int_tol, reltol = time_int_tol,
 #             ode_default_options()..., callback = callbacks)
 summary_callback()
+
 # Plotting
 # using GLMakie
 # time = 0.0
