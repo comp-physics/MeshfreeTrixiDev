7c062ee68f73055705db67798e1a14c164ce0b39
jarias9 <68081892+jarias9@users.noreply.github.com>
2024-04-06 21:23:35 -0400

    Added 0.01 geometry

--- examples/pointcloud_2d/supersonic_cylinder.jl
+++ examples/pointcloud_2d/supersonic_cylinder.jl
@@ -6,18 +6,23 @@ using OrdinaryDiffEq
 
 # Base Methods
 approximation_order = 3
-rbf_order = 5
+rbf_order = 3
 # Specialized Methods
 basis = PointCloudBasis(Point2D(), approximation_order;
                         approximation_type = RBF(PolyharmonicSpline(rbf_order)))
 solver = PointCloudSolver(basis)
 
 dir = "./medusa_point_clouds"
-casename = "cyl_0_00625"
+casename = "cyl_0_01"
 domain_name = joinpath(dir, casename)
 savename = casename * "_order_$approximation_order"
 boundary_names = Dict(:inlet => 1, :outlet => 2, :bottom => 3, :top => 4, :cyl => 5)
 domain = PointCloudDomain(solver, domain_name, boundary_names)
+scatter(domain.pd.points, axis = (aspect = DataAspect(),))
+for tag in keys(domain.boundary_tags)
+    idx = domain.boundary_tags[tag].idx
+    scatter!(domain.pd.points[idx], label = string(tag))
+end
 
 equations = CompressibleEulerEquations2D(1.4)
 function initial_condition_cyl(x, t, equations::CompressibleEulerEquations2D)
@@ -35,11 +40,11 @@ boundary_conditions = (; :inlet => BoundaryConditionDirichlet(initial_condition)
                        :cyl => boundary_condition_slip_wall)
 
 # Test upwind viscosity
-source_rv = SourceResidualViscosityTominec(solver, equations, domain; c_rv = 1.0,
-                                           c_uw = 1.0, polydeg = approximation_order + 1)
+source_rv = SourceResidualViscosityTominec(solver, equations, domain; c_rv = 5,
+                                           c_uw = 1.0, polydeg = approximation_order)
 # source_rv = SourceUpwindViscosityTominec(solver, equations, domain; c_uw = 1.0)
 source_hv = SourceHyperviscosityTominec(solver, equations, domain;
-                                        c = domain.pd.dx_min^(-2 + 0.0))
+                                        c = domain.pd.dx_min^(-2 + 0.5))
 # source_hv = SourceHyperviscosityFlyer(solver, equations, domain;
 #                                       k = 2,
 #                                       c = domain.pd.dx_min^(-2 + 0.0))
@@ -55,7 +60,7 @@ ode = semidiscretize(semi, tspan)
 # summary_callback = SummaryCallback()
 summary_callback = InfoCallback()
 alive_callback = AliveCallback(alive_interval = 10)
-history_callback = HistoryCallback(approx_order = approximation_order + 1)
+history_callback = HistoryCallback(approx_order = approximation_order)
 # analysis_interval = 100
 # analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
 save_solution = SolutionSavingCallback(dt = 0.01,
